f0e10ae38c7261648f4ab5415b6a4363eabcd542
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-06-19 19:04:25 +0200

    :recycle: Refactor methods to Objects

--- src/gilded_rose/gilded_rose.py
+++ src/gilded_rose/gilded_rose.py
@@ -1,20 +1,21 @@
 from __future__ import annotations
+from abc import abstractmethod
 
 class GildedRose(object):
 
     def __init__(self, items):
-        self.items = items
+        self.items: list[Item] = []
+        for item in items:
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.items.append(BackstagePasses(name=item.name, sell_in=item.sell_in, quality=item.quality))
+            elif item.name == "Aged Brie":
+                self.items.append(AgedBrie(name=item.name, sell_in=item.sell_in, quality=item.quality))
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                self.items.append(Sulfuras(name=item.name, sell_in=item.sell_in, quality=item.quality))
 
     def update_quality(self):
         for item in self.items:
-            if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                update_backstage_passes_concert_quality(item=item)
-                continue
-            if item.name == "Aged Brie":
-                update_aged_brie_quality(item=item)
-                continue
-            if item.name == "Sulfuras, Hand of Ragnaros":
-                continue
+            item.update_quality()
 
 def update_aged_brie_quality(item: Item):
     item.quality += 1
@@ -48,5 +49,39 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    @abstractmethod
+    def update_quality(self):
+        raise NotImplementedError
+
+
+class Sulfuras(Item):
+    def update_quality(self):
+        pass
+
+class AgedBrie(Item):
+    def update_quality(self):
+        self.quality += 1
+
+        if self.sell_in <= 0:
+            self.quality += 1
+        if self.quality > 50:
+            self.quality = 50
+        self.sell_in -= 1
+
+class BackstagePasses(Item):
+    def update_quality(self):
+        self.quality += 1
+
+        if self.sell_in <= 10:
+            self.quality += 1
+        if self.sell_in <= 5:
+            self.quality += 1
+
+        if self.quality > 50:
+            self.quality = 50
+
+        if self.sell_in <= 0:
+            self.quality = 0
+
+        self.sell_in -= 1
+
