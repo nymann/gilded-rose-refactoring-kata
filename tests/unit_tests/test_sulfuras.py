36c9c7f934cca26f77a58322bfed1fc30f57263f
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-06-19 17:44:47 +0200

    :white_check_mark: Add negative expire day test Sulfuras
    This is merely to cover a logic branch more and should be refactored
    later, since we don't need it by the spec

--- tests/unit_tests/test_sulfuras.py
+++ tests/unit_tests/test_sulfuras.py
@@ -6,3 +6,10 @@ def test_sulfuras():
     gilded_rose.update_quality()
     assert sulfuras.quality == 80
     assert sulfuras.sell_in == 10
+
+def test_sulfuras_negative():
+    sulfuras = Item(name="Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)
+    gilded_rose = GildedRose(items=[sulfuras])
+    gilded_rose.update_quality()
+    assert sulfuras.quality == 80
+    assert sulfuras.sell_in == -1
