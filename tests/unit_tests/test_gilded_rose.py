a8db0d45edb068af29a35416b4008b7cf6a994c4
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-06-19 22:21:30 +0200

    :white_check_mark: Actually test something in test_gilded_rose.py

--- tests/unit_tests/test_gilded_rose.py
+++ tests/unit_tests/test_gilded_rose.py
@@ -1,20 +1,21 @@
 from gilded_rose.gilded_rose import GildedRose
-from gilded_rose.items.aged_brie import AgedBrie
-from gilded_rose.items.conjured import ConjuredItem
 from gilded_rose.items.item import Item
 
-COMMON_VARS = {"sell_in": 10, "quality": 5}
-items = [
-        Item(name="Sulfuras, Hand of Ragnaros", **COMMON_VARS),
-        AgedBrie(name="Aged Brie", **COMMON_VARS),
-        ConjuredItem(name="Conjured", **COMMON_VARS),
-        ConjuredItem(name="Backstage passes to a TAFKAL80ETC concert", **COMMON_VARS)
-]
+items = [Item(name="Aged Brie", sell_in=2, quality=4)]
 
 def test_init():
-    """Test if no errors are thrown.
+    """Test if all items are converted.
 
-    Since we are testing all the items by themselves we don't really need to do anything.
+    We test that that the converted type is correct in test_item_factory so we don't do it here.
     """
+
+    gilded_rose = GildedRose(items=items)
+    assert len(gilded_rose.items) == len(items)
+
+def test_update_call():
+    """Test that update quality is called on the GildedRose items."""
+
     gilded_rose = GildedRose(items=items)
     gilded_rose.update_quality()
+    aged_brie = gilded_rose.items[0]
+    assert aged_brie.quality == 5
