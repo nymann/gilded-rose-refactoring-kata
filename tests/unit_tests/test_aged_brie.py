67103b324055a4ab7dac655b1306adc6281917ea
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-06-19 17:45:11 +0200

    :white_check_mark: Implement aged_brie test based on requirements

--- tests/unit_tests/test_aged_brie.py
+++ tests/unit_tests/test_aged_brie.py
@@ -1,14 +1,13 @@
 from gilded_rose.gilded_rose import GildedRose, Item
 
 def test_aged_brie():
-    aged_brie = Item(name="Aged Brie", sell_in=10, quality=5)
+    aged_brie = Item(name="Aged Brie", sell_in=20, quality=5)
     gilded_rose = GildedRose(items=[aged_brie])
-    for _ in range(30):
-        if aged_brie.sell_in == 0:
-            expected = aged_brie.quality + 2
-        else:
-            expected = aged_brie.quality + 1
-        if expected > 50:
-            expected = 50
+    for _ in range(100):
+        expected_quality = aged_brie.quality + 1
+        if aged_brie.sell_in <= 0:
+            expected_quality += 1
+        if expected_quality > 50:
+            expected_quality = 50
         gilded_rose.update_quality()
-        aged_brie.quality == expected
+        assert aged_brie.quality == expected_quality
