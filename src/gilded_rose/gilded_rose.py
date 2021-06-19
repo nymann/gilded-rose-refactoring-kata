7a9f68c546c534bbed214e601cc0d7da662bebe4
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-06-19 19:07:39 +0200

    :recycle: Move common sell_in behaviour to the top

--- src/gilded_rose/gilded_rose.py
+++ src/gilded_rose/gilded_rose.py
@@ -45,16 +45,15 @@ class AgedBrie(Item):
 class BackstagePasses(Item):
     def update_quality(self):
         self.quality += 1
+        self.sell_in -= 1
 
-        if self.sell_in <= 10:
+        if self.sell_in < 10:
             self.quality += 1
-        if self.sell_in <= 5:
+        if self.sell_in < 5:
             self.quality += 1
 
         if self.quality > 50:
             self.quality = 50
 
-        if self.sell_in <= 0:
+        if self.sell_in < 0:
             self.quality = 0
-
-        self.sell_in -= 1
