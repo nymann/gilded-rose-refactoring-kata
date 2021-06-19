2a8402d409869ab53f486dcaa4f494d1018bdfc7
Kristian Nymann Jakobsen <kristian@nymann.dev>
2021-06-19 17:01:34 +0200

    :white_check_mark: Finalise Sulfuras test
    There's no need to loop in the Sulfuras test. Since we are already
    testing all the logic branches (inspected via Coverage report)

--- tests/unit_tests/test_sulfuras.py
+++ tests/unit_tests/test_sulfuras.py
@@ -3,7 +3,6 @@ from gilded_rose.gilded_rose import GildedRose, Item
 def test_sulfuras():
     sulfuras = Item(name="Sulfuras, Hand of Ragnaros", sell_in=10, quality=80)
     gilded_rose = GildedRose(items=[sulfuras])
-    for _ in range(30):
-        gilded_rose.update_quality()
-        assert sulfuras.quality == 80
-        assert sulfuras.sell_in == 10
+    gilded_rose.update_quality()
+    assert sulfuras.quality == 80
+    assert sulfuras.sell_in == 10
